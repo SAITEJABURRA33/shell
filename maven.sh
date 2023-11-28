6617c3621c705a85bd7b9f0b854b8356bec398d8
SAITEJABURRA33 <144418626+SAITEJABURRA33@users.noreply.github.com>
2023-11-29 00:14:08 +0530

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -4,7 +4,7 @@ sudo yum update
 sudo yum install git -y
 sudo yum install maven -y
 sudo yum install java -y
-sudo git clone https://github.com/SAITEJABURRA33/gctech.git 
+sudo git clone https://github.com/SAITEJABURRA33/gctech.git
 cd gctech
 mvn clean install
 if [ $? -eq 0 ]; then
@@ -14,8 +14,33 @@ else
     exit 1  # Exit with an error code
 fi
 cd
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.83/bin/apache-tomcat-9.0.83.tar.gz
-sudo tar -xzvf apache-tomcat-9.0.83.tar.gz
-sudo mv /root/gctech/target/*.war /root/apache-tomcat-9.0.83/webapps/
-cd /apache-tomcat-9.0.83/bin
-./startup.sh
+#!/bin/bash
+
+# Specify Tomcat version
+TOMCAT_VERSION="9.0.60"  # Update this to the desired version
+
+# Specify installation directory
+INSTALL_DIR="/opt/tomcat"
+
+# Download and extract Tomcat
+wget "https://archive.apache.org/dist/tomcat/tomcat-9/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz" -P /tmp
+sudo mkdir -p ${INSTALL_DIR}
+sudo tar xf /tmp/apache-tomcat-${TOMCAT_VERSION}.tar.gz -C ${INSTALL_DIR} --strip-components=1
+
+# Set permissions
+sudo chgrp -R root ${INSTALL_DIR}
+sudo chmod -R 755 ${INSTALL_DIR}/bin
+sudo chmod -R 755 ${INSTALL_DIR}/conf
+sudo chmod -R 755 ${INSTALL_DIR}/lib
+sudo chmod -R 755 ${INSTALL_DIR}/logs
+sudo chmod -R 755 ${INSTALL_DIR}/temp
+sudo chmod -R 755 ${INSTALL_DIR}/webapps
+sudo chmod -R 755 ${INSTALL_DIR}/work
+
+# Start Tomcat
+${INSTALL_DIR}/bin/startup.sh
+
+echo "Tomcat installation and startup complete."
+sudo mv /root/shell/gctech/target/*.war /root/apache-tomcat-9.0.83/webapps
+                                                                                                                                                    
+
